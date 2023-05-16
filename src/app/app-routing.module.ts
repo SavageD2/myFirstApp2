fae6abc5befe472a3efe8e00db4bd6fa7bf8447a
Marvin <bourhanemarvin@hotmail.fr>
2023-05-16 16:50:26 +0200

    routes are working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from 'src/sign-up/sign-up.component';
 
-const routes: Routes = [];
+import { UserProfileComponent } from 'src/user-profile/user-profile.component';
+
+const routes: Routes = [
+  { path: '', redirectTo:'/user', pathMatch:'full'},
+  { path: 'signup', component: SignUpComponent },
+  { path: 'user', component: UserProfileComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { FormsModule } from '@angular/forms';
 import { DirectivesComponent } from './directives/directives.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from '../sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockComponent,
     UserProfileComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    MenuComponent,
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
