f8a06a8dce1c0ec2d12529a57fd313096012626f
Marvin <bourhanemarvin@hotmail.fr>
2023-05-16 17:06:44 +0200

    my form is litteral perfection

--- src/sign-up/sign-up.component.ts
+++ src/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormDetail } from 'src/app/models/form.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  signUpForm: FormDetail = new FormDetail('','','','');
+  formSubmitted: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(myForm:NgForm){
+    this.formSubmitted = true;
+    console.log(this.signUpForm)
   }
 
 }
